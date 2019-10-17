ae10e701d7fb0ee57ab8c19582e2af1a506838f8
brrastak <54371570+brrastak@users.noreply.github.com>
2019-10-17 15:48:43 +0300

    Update SPI.c

--- SPI.c
+++ SPI.c
@@ -1,10 +1,3 @@
-/*
-    1. Configure pins as alternate function push-pull
-    2. Init RCC: RCC->APB1ENR |= RCC_APB1ENR_USART_EN;
-    3. Enable interrupt
-    4. Call function InitUART
-*/
-
 // STM32F103
 #include "stm32f10x.h"
 
@@ -17,31 +10,29 @@
 
 // Local variables
 // Data
-uint8_t* transmit_buf;
-uint8_t* receive_buf;
+static uint16_t* transmit_buf;
+static uint16_t* receive_buf;
 // Number of data
-int to_transmit = 0;
-int to_receive = 0;
+static int to_transmit = 0;
+static int to_receive = 0;
 // Flags
-volatile bool transmitted = true;
-volatile bool received = true;
+static volatile bool received = true;
 
 // Interrupt
-void USART2_IRQHandler(void)
+void SPI1_IRQHandler(void)
 {
     // Transmit data register empty
-    if ( ((USART2->SR & USART_SR_TXE) != 0) && (to_transmit != 0) ) {
-        USART2->DR = transmit_buf[0];
-        transmit_buf++;
-        to_transmit--;
-    }
-    // Transmission complete
-    if ((USART2->SR & USART_SR_TC) != 0) {
-        transmitted = true;
-        USART2->CR1 &= ~USART_CR1_TXEIE;    // Transmit data register empty interrupt disable
+    if ( (SPI1->SR & SPI_SR_TXE) != 0 ) {
+        if (to_transmit != 0) {
+            SPI1->DR = transmit_buf[0];
+            transmit_buf++;
+            to_transmit--;
+        }
+        else
+            SPI1->CR2 &= ~SPI_CR2_TXEIE;    // TX buffer empty interrupt disable
     }
     // Received data register not empty
-    if ((USART2->SR & USART_CR1_RXNEIE) != 0) {
+    if ((SPI1->SR & SPI_SR_RXNE) != 0) {
     }
 }
 
@@ -56,31 +47,37 @@ void InitSPI(void)
                     SPI_CR1_DFF         * 1 |   // 16-bit data frame format
                     SPI_CR1_LSBFIRST    * 0 |   // MSB transmitted first
                     SPI_CR1_MSTR        * 1 |   // master mode
-                    SPI_CR1_SSM         * 1 |   // software NSS
+                    SPI_CR1_SSM         * 1 |   // software NSS enable
                     SPI_CR1_SSI         * 1;    // software NSS up
-    //SPI1->CR2 =     SPI_CR2_SSOE        * 1;    // SS output enable
+    SPI1->CR2 =     SPI_CR2_SSOE        * 1;    // SS output enable
     SPI1->CR1 |=    SPI_CR1_SPE;    // SPI enable
+    
+    ChipDeselectSPI();
 }
-void TransmitSPI(uint8_t* buf, int num)
+void TransmitSPI(uint16_t* buf, int num)
 {
     transmit_buf = buf;
     to_transmit = num - 1;
-    transmitted = false;
     
-    USART2->DR = transmit_buf[0];
-    USART2->CR1 |=  USART_CR1_TXEIE;    // Transmit data register empty interrupt enable
+    ChipSelectSPI();
+    
+    SPI1->DR = transmit_buf[0];
+    SPI1->CR2 |= SPI_CR2_TXEIE;     // TX buffer empty interrupt enable
     
     transmit_buf++;
 }
-void ReceiveSPI(uint8_t* buf, int num)
+void ReceiveSPI(uint16_t* buf, int num)
 {
     receive_buf = buf;
     to_receive = num;
     received = false;
+    
+    // In software NSS mode
+    //ChipSelectSPI();
 }
 bool SPItransmitted(void)
 {
-    return transmitted;
+    return ((SPI1->SR & SPI_SR_BSY) == 0);
 }
 bool SPIreceived(void)
 {
@@ -88,7 +85,9 @@ bool SPIreceived(void)
 }
 void ChipSelectSPI(void)
 {
+    PinReset(SPI_PORT, NSS_PIN);
 }
 void ChipDeselectSPI(void)
 {
+    PinSet(SPI_PORT, NSS_PIN);
 }
