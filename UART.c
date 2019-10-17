c4467ceb38cb099bfc98ef66856c4bedd383aaa6
brrastak <54371570+brrastak@users.noreply.github.com>
2019-10-17 14:06:02 +0300

    Update UART.c

--- UART.c
+++ UART.c
@@ -57,9 +57,9 @@ void TransmitUART(uint8_t* buf, int num)
     to_transmit = num - 1;
     transmitted = false;
     
-    USART2->DR = transmit_buf[0];
-    USART2->CR1 |=  USART_CR1_TXEIE;    // Transmit data register empty interrupt enable
+    USART2->CR1 |= USART_CR1_TXEIE;    // Transmit data register empty interrupt enable
     USART2->CR1 |= USART_CR1_TCIE;      // Transmission complete interrupt enable
+    USART2->DR = transmit_buf[0];
     
     transmit_buf++;
 }
