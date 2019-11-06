9c95be27a49ecb68a286a4f162cf81184878f3d5
brrastak <54371570+brrastak@users.noreply.github.com>
2019-11-06 13:48:30 +0300

    Add files via upload

--- WS2812B.c
+++ WS2812B.c
@@ -5,3 +5,44 @@
 // user
 #include "WS2812B.h"
 
+typedef struct
+{
+    uint8_t g[DIGIT_NUM];
+    uint8_t r[DIGIT_NUM];
+    uint8_t b[DIGIT_NUM];
+} rgb_led_t;
+
+static rgb_led_t LED[LED_NUM] = {DUTY_LOW};
+uint8_t * digit = (uint8_t *)&LED;
+static int digit_counter = 0;
+
+void CodeRGB(int red, int green, int blue, int num)
+{
+    int i;
+    
+    for (i = 0; i < DIGIT_NUM; i++) {
+        if ((red & (1 << i)) != 0)
+            LED[num].r[DIGIT_NUM-1-i] = DUTY_HIGH;
+        else
+            LED[num].r[DIGIT_NUM-1-i] = DUTY_LOW;
+        if ((green & (1 << i)) != 0)
+            LED[num].g[DIGIT_NUM-1-i] = DUTY_HIGH;
+        else
+            LED[num].g[DIGIT_NUM-1-i] = DUTY_LOW;
+        if ((blue & (1 << i)) != 0)
+            LED[num].b[DIGIT_NUM-1-i] = DUTY_HIGH;
+        else
+            LED[num].b[DIGIT_NUM-1-i] = DUTY_LOW;
+    }
+}
+inline uint8_t DutyTimeValue()
+{
+    return digit[digit_counter++];
+}
+inline bool AllDigits()
+{
+    bool res = (digit_counter == LED_NUM * DIGIT_NUM * COLOR_NUM);
+    if (res)
+        digit_counter = 0;
+    return res;
+}
--- WS2812B.h
+++ WS2812B.h
@@ -9,6 +9,8 @@
         duty time for logical "0": 0,4 mks
         duty time for logical "1": 0,8 mks
         period: 1,25 mks
+    3. The order of colors is green, red, blue
+    4. The order of digits is MSB first
 */
 
 // cstd
@@ -18,6 +20,18 @@
 #ifndef __WS2812B_H__
 #define __WS2812B_H__
 
+#define LED_NUM                 8   // number of LEDs
+#define DIGIT_NUM               8   // number of binary digits
+#define COLOR_NUM               3   // number of colors
+// Duty time for 0 and 1 values for LED
+#define DUTY_LOW                3
+#define DUTY_HIGH               6
 
+// Code three color values into LED structure
+void CodeRGB(int red, int green, int blue, int num);
+// Return duty time for next binary digit
+uint8_t DutyTimeValue(void);
+// If all digits are send
+bool AllDigits(void);
 
 #endif
