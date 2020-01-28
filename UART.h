46aa150ce0aa22f461b3cc90661b0583e3ad33f7
brrastak <54371570+brrastak@users.noreply.github.com>
2020-01-28 10:35:09 +0300

    Update UART.h

--- UART.h
+++ UART.h
@@ -17,9 +17,11 @@
 #define __UART_H__
 
 // Baud rate for USART->BRR register
-#define USART9600           0xEA6       // 9600 bit per second
-#define USART57600          0x271       // 57600 bit per second
-#define USART115200         0x138       // 115200 bit per second
+// Calculated as 36MHz divided by desired baud rate
+#define USART9600           3750        // 9600 bit per second
+#define USART57600          625         // 57600 bit per second
+#define USART115200         312         // 115200 bit per second
+#define USART460800         78          // 460800 bit per second
 
 void USART2_IRQHandler(void);
 
