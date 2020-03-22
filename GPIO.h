6e1601808934b28483fb1d350cb992de51da5099
brrastak <54371570+brrastak@users.noreply.github.com>
2020-03-22 20:24:38 +0300

    Update GPIO.h

--- GPIO.h
+++ GPIO.h
@@ -16,12 +16,16 @@
 #define PIN_MODE_50MHZ_OPP      0x03    // out push-pull; speed 50 MHz
 
 #define PIN_MODE_2MHZ_OOD       0x06    // out open-drain; speed 2 MHz
+#define PIN_MODE_10MHZ_OOD      0x05    // out open-drain; speed 10 MHz
+#define PIN_MODE_50MHZ_OOD      0x07    // out open-drain; speed 50 MHz
 
 #define PIN_MODE_2MHZ_AFPP      0x0A    // alternate function push-pull; speed 2 MHz
 #define PIN_MODE_10MHZ_AFPP     0x09    // alternate function push-pull; speed 10 MHz
 #define PIN_MODE_50MHZ_AFPP     0x0B    // alternate function push-pull; speed 50 MHz
 
 #define PIN_MODE_2MHZ_AFOD      0x0E    // alternate function open-drain; speed 2 MHz
+#define PIN_MODE_10MHZ_AFOD     0x0D    // alternate function open-drain; speed 10 MHz
+#define PIN_MODE_50MHZ_AFOD     0x0F    // alternate function open-drain; speed 50 MHz
 
 // GPIO
 void SetGpioMode(GPIO_TypeDef* port, int pin, int mode);
