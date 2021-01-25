be3dffbf26509e8cb8c05c498ff09c2ab5124010
brrastak <54371570+brrastak@users.noreply.github.com>
2021-01-25 16:50:52 +0300

    Add files via upload

--- i2c.c
+++ i2c.c
@@ -1,49 +1,89 @@
 // STM32F103
 #include "stm32f10x.h"
 
+// cstd
+#include <stdbool.h>
+
 // user
 #include "i2c.h"
 
-uint8_t i2c_addr;
-uint8_t i2c_data;
-int i2c_byte_to_send;
-
-void InitI2C(void)
-{
-    I2C2->CR2 = 36;                     // RCC 36MHz
-    I2C2->CCR |= 180;                   // 100kHz
-    I2C2->TRISE = 0x55;                 // 1000ns
-    I2C2->CR1 |= I2C_CR1_PE;            // peripheral enable
-    I2C2->CR2 |=    I2C_CR2_ITEVTEN |   // event interrupt enable
-                    I2C_CR2_ITBUFEN;    // buffer interrupt enable
-}
-
-inline void I2CWrite(uint8_t addr, uint8_t data, int num)
-{
-    i2c_byte_to_send = num;
-    i2c_addr = addr;
-    i2c_data = data;
-    I2C2->CR1 |= I2C_CR1_START;
-}
+// Local variables
+// Data
+static uint8_t* transmit_buf;
+//static uint8_t* receive_buf;
+// Data transmit address
+static uint8_t addr;
+// Number of data
+static int to_transmit = 0;
+//static int to_receive = 0;
+// Flags
+static volatile bool transmitted = true;
+static volatile bool received = true;
 
 // I2C interrupt
-void I2C2_EV_IRQHandler(void)
+void I2C1_EV_IRQHandler(void)
 {   
-    if (I2C2->SR1 | I2C_SR1_SB) {
-        (void)I2C2->SR1;
-        I2C2->DR = i2c_addr;
+    // Start condition generated
+    if ((I2C1->SR1 | I2C_SR1_SB) != 0) {
+        
+        (void)I2C1->SR1;                // clear flags
+        I2C1->DR = addr;                // send I2C address
     }
-    if (I2C2->SR1 | I2C_SR1_ADDR) {
-        (void)I2C2->SR1;
-        (void)I2C2->SR2;
-        I2C2->DR = i2c_data;
+    // Address sent
+    if ((I2C1->SR1 | I2C_SR1_ADDR) != 0) {
+        
+        (void)I2C1->SR1;                // clear flags
+        (void)I2C1->SR2;
+        I2C1->DR = transmit_buf[0];     // send first data byte
     }
-    if ((I2C2->SR1 | I2C_SR1_BTF)&&(I2C2->SR1 | I2C_SR1_TXE)) {
-        (void)I2C2->SR1;
-        if (i2c_byte_to_send == 0) {
-            I2C2->CR1 |= I2C_CR1_STOP;
+    // Byte transfer finished and transmitter register empty
+    if (((I2C1->SR1 | I2C_SR1_BTF)&&(I2C1->SR1 | I2C_SR1_TXE)) != 0) {
+        
+        (void)I2C1->SR1;                // clear flags
+        if (to_transmit == 0) {         // no more data to send
+            
+            I2C1->CR1 |= I2C_CR1_STOP;  // generate stop condition
+            transmitted = true;
         }
         else
-            i2c_byte_to_send--;
+            to_transmit--;
     }
+}
+void InitI2c(void)
+{
+    // Transmitter in master mode
+    I2C1->CR2 = 36;                     // RCC 36MHz
+    I2C1->CCR |= 180;                   // 100kHz
+    I2C1->TRISE = 0x55;                 // 1000ns
+    I2C1->CR1 |= I2C_CR1_PE;            // peripheral enable
+    I2C1->CR2 |=    I2C_CR2_ITEVTEN |   // event interrupt enable
+                    I2C_CR2_ITBUFEN;    // buffer interrupt enable
+}
+void TransmitI2c(uint8_t i2c_addr, uint8_t* buf, int num)
+{
+    to_transmit = num;
+    addr = i2c_addr;
+    transmit_buf = buf;
+    transmitted = false;
+    
+    // Generate start condition
+    I2C1->CR1 |= I2C_CR1_START;
+}
+bool TransmittedI2c(void)
+{
+    return transmitted;
+}
+bool ReceivedI2c(void)
+{
+    return received;
+}
+void WaitForTransmitI2c(void)
+{
+    while (! transmitted)
+        ;
+}
+void WaitForReceiveI2c(void)
+{
+    while (! received)
+        ;
 }
--- i2c.h
+++ i2c.h
@@ -1,12 +1,27 @@
+/*
+    1. Configure pins as alternate function open drain
+    2. Init RCC: RCC->APB1ENR |= RCC_APB1ENR_I2CxEN;
+    3. Enable interrupt: NVIC_EnableIRQ(I2Cx_EV_IRQn);
+    4. Call function InitI2c();
+*/
+
 // STM32F103
 #include "stm32f10x.h"
 
 #ifndef __I2C_H__
 #define __I2C_H__
 
-void InitI2C(void);
-void I2CWrite(uint8_t addr, uint8_t data, int num);
 
-void I2C2_EV_IRQHandler(void);
+void InitI2c(void);
+
+void TransmitI2c(uint8_t i2c_addr, uint8_t* buf, int num);
+
+bool TransmittedI2c(void);
+bool ReceivedI2c(void);
+
+void WaitForTransmitI2c(void);
+void WaitForReceiveI2c(void);
+
+
 
 #endif
