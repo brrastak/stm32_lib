55aca7c9d364c09052f1877c1d510b2b4ac504b1
brrastak <54371570+brrastak@users.noreply.github.com>
2021-03-25 13:54:04 +0300

    Add files via upload

--- i2c.c
+++ i2c.c
@@ -6,6 +6,8 @@
 
 // user
 #include "i2c.h"
+#include "main.h"
+#include "gpio.h"
 
 // Local variables
 // Data
@@ -21,43 +23,53 @@ static volatile bool transmitted = true;
 static volatile bool received = true;
 
 // I2C interrupt
-void I2C1_EV_IRQHandler(void)
+void I2C2_EV_IRQHandler(void)
 {   
     // Start condition generated
-    if ((I2C1->SR1 | I2C_SR1_SB) != 0) {
+    if ((I2C2->SR1 & I2C_SR1_SB) != 0) {
         
-        (void)I2C1->SR1;                // clear flags
-        I2C1->DR = addr;                // send I2C address
+        (void)I2C2->SR1;                // clear flags
+        I2C2->DR = addr;                // send I2C address
     }
     // Address sent
-    if ((I2C1->SR1 | I2C_SR1_ADDR) != 0) {
+    if ((I2C2->SR1 & I2C_SR1_ADDR) != 0) {
         
-        (void)I2C1->SR1;                // clear flags
-        (void)I2C1->SR2;
-        I2C1->DR = transmit_buf[0];     // send first data byte
+        (void)I2C2->SR1;                // clear flags
+        (void)I2C2->SR2;
+        I2C2->DR = transmit_buf[0];     // send first data byte
+        transmit_buf++;
+        to_transmit--;
     }
-    // Byte transfer finished and transmitter register empty
-    if (((I2C1->SR1 | I2C_SR1_BTF)&&(I2C1->SR1 | I2C_SR1_TXE)) != 0) {
+    // Transmitter register empty
+    if ((I2C2->SR1 & I2C_SR1_TXE) != 0) {
         
-        (void)I2C1->SR1;                // clear flags
         if (to_transmit == 0) {         // no more data to send
-            
-            I2C1->CR1 |= I2C_CR1_STOP;  // generate stop condition
-            transmitted = true;
+
+            I2C2->CR2 &= ~I2C_CR2_ITBUFEN;      // buffer interrupt disable
         }
-        else
+        else {
+            
+            I2C2->DR = transmit_buf[0];         // send data byte
+            transmit_buf++;
             to_transmit--;
+        }
     }
+    // Transmission completed
+    if (((I2C2->SR1 & I2C_SR1_BTF) != 0) && (to_transmit == 0)) {
+        
+        I2C2->CR2 &= ~I2C_CR2_ITEVTEN;      // event interrupt disable
+        I2C2->CR1 |= I2C_CR1_STOP;          // generate stop condition
+        transmitted = true;
+    }
+
 }
 void InitI2c(void)
 {
     // Transmitter in master mode
-    I2C1->CR2 = 36;                     // RCC 36MHz
-    I2C1->CCR |= 180;                   // 100kHz
-    I2C1->TRISE = 0x55;                 // 1000ns
-    I2C1->CR1 |= I2C_CR1_PE;            // peripheral enable
-    I2C1->CR2 |=    I2C_CR2_ITEVTEN |   // event interrupt enable
-                    I2C_CR2_ITBUFEN;    // buffer interrupt enable
+    I2C2->CR2 = 36;                     // RCC 36MHz
+    I2C2->CCR |= 180;                   // 100kHz
+    I2C2->TRISE = 0x55;                 // 1000ns
+    I2C2->CR1 |= I2C_CR1_PE;            // peripheral enable
 }
 void TransmitI2c(uint8_t i2c_addr, uint8_t* buf, int num)
 {
@@ -66,8 +78,11 @@ void TransmitI2c(uint8_t i2c_addr, uint8_t* buf, int num)
     transmit_buf = buf;
     transmitted = false;
     
+    I2C2->CR2 |=    I2C_CR2_ITEVTEN |   // event interrupt enable
+                    I2C_CR2_ITBUFEN;    // buffer interrupt enable
+    
     // Generate start condition
-    I2C1->CR1 |= I2C_CR1_START;
+    I2C2->CR1 |= I2C_CR1_START;
 }
 bool TransmittedI2c(void)
 {
